f5f2821d529d8ace5d64bb21129df147ff298df1
SatooRu65536 <ryuto65536@gmail.com>
2025-04-08 08:01:07 +0900

    fix: vite config

---
+++ api/vite.config.ts
@@ -0,0 +1,16 @@
+import build from '@hono/vite-build/cloudflare-pages';
+import devServer from '@hono/vite-dev-server';
+import adapter from '@hono/vite-dev-server/cloudflare';
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig({
+  plugins: [
+    tsconfigPaths(),
+    build(),
+    devServer({
+      adapter,
+      entry: 'src/index.ts',
+    }),
+  ],
+});
