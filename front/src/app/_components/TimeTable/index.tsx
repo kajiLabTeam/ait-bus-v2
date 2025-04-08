0f8a2251500c6b79d0db8a3d645e5d5b13fa40c3
SatooRu65536 <ryuto65536@gmail.com>
2025-04-08 21:06:24 +0900

    fix: tableタグ

--- front/src/app/_components/TimeTable/index.tsx
+++ front/src/app/_components/TimeTable/index.tsx
@@ -28,156 +28,171 @@ export default function TimeTable() {
         <h3>時刻表</h3>
 
         <table data-active={mode === 'A'}>
-          <tr>
-            <th>時</th>
-            <th>分</th>
-            <th>時</th>
-            <th>分</th>
-          </tr>
-          <tr>
-            <td width="10%">8</td>
-            <td width="40%">00　05　10　15　20　25　30　35　40　45　50　55</td>
-            <td width="10%">15</td>
-            <td width="40%">05　15　30　45</td>
-          </tr>
-          <tr>
-            <td>9</td>
-            <td>00　05　10　15　20　25　30　35　40　50　55</td>
-            <td>16</td>
-            <td>00　15　30　45</td>
-          </tr>
-          <tr>
-            <td>10</td>
-            <td>00　05　10　15　20　25　30　35　45　55</td>
-            <td>17</td>
-            <td>00　10　25　40</td>
-          </tr>
-          <tr>
-            <td>11</td>
-            <td>05　15　25　35　45　55</td>
-            <td>18</td>
-            <td>00　15　45</td>
-          </tr>
-          <tr>
-            <td>12</td>
-            <td>05　15　25　35　45　55</td>
-            <td>19</td>
-            <td>00　15　30　45</td>
-          </tr>
-          <tr>
-            <td>13</td>
-            <td>05　20　35　50</td>
-            <td>20</td>
-            <td>00　30</td>
-          </tr>
-          <tr>
-            <td>14</td>
-            <td>05　15　25　35　45　55</td>
-            <td>21</td>
-            <td>00</td>
-          </tr>
+          <thead>
+            <tr>
+              <th>時</th>
+              <th>分</th>
+              <th>時</th>
+              <th>分</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <td width="10%">8</td>
+              <td width="40%">00　05　10　15　20　25　30　35　40　45　50　55</td>
+              <td width="10%">15</td>
+              <td width="40%">05　15　30　45</td>
+            </tr>
+            <tr>
+              <td>9</td>
+              <td>00　05　10　15　20　25　30　35　40　50　55</td>
+              <td>16</td>
+              <td>00　15　30　45</td>
+            </tr>
+            <tr>
+              <td>10</td>
+              <td>00　05　10　15　20　25　30　35　45　55</td>
+              <td>17</td>
+              <td>00　10　25　40</td>
+            </tr>
+            <tr>
+              <td>11</td>
+              <td>05　15　25　35　45　55</td>
+              <td>18</td>
+              <td>00　15　45</td>
+            </tr>
+            <tr>
+              <td>12</td>
+              <td>05　15　25　35　45　55</td>
+              <td>19</td>
+              <td>00　15　30　45</td>
+            </tr>
+            <tr>
+              <td>13</td>
+              <td>05　20　35　50</td>
+              <td>20</td>
+              <td>00　30</td>
+            </tr>
+            <tr>
+              <td>14</td>
+              <td>05　15　25　35　45　55</td>
+              <td>21</td>
+              <td>00</td>
+            </tr>
+          </tbody>
         </table>
 
         <table data-active={mode === 'B'}>
-          <tr>
-            <th>時</th>
-            <th>分</th>
-            <th>時</th>
-            <th>分</th>
-          </tr>
-          <tr>
-            <td width="10%">8</td>
-            <td width="40%">00　10　20　35　45</td>
-            <td width="10%">15</td>
-            <td width="40%">05　35</td>
-          </tr>
-          <tr>
-            <td>9</td>
-            <td>00　05　25　35　50</td>
-            <td>16</td>
-            <td>05　45</td>
-          </tr>
-          <tr>
-            <td>10</td>
-            <td>00　10　30　55</td>
-            <td>17</td>
-            <td>10　45</td>
-          </tr>
-          <tr>
-            <td>11</td>
-            <td>00　25　50</td>
-            <td>18</td>
-            <td>05　35</td>
-          </tr>
-          <tr>
-            <td>12</td>
-            <td>25</td>
-            <td>19</td>
-            <td>35</td>
-          </tr>
-          <tr>
-            <td>13</td>
-            <td>35</td>
-            <td>20</td>
-            <td>25</td>
-          </tr>
-          <tr>
-            <td>14</td>
-            <td>05　35</td>
-            <td>21</td>
-            <td>05</td>
-          </tr>
+          <thead>
+            <tr>
+              <th>時</th>
+              <th>分</th>
+              <th>時</th>
+              <th>分</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <td width="10%">8</td>
+              <td width="40%">00　10　20　35　45</td>
+              <td width="10%">15</td>
+              <td width="40%">05　35</td>
+            </tr>
+            <tr>
+              <td>9</td>
+              <td>00　05　25　35　50</td>
+              <td>16</td>
+              <td>05　45</td>
+            </tr>
+            <tr>
+              <td>10</td>
+              <td>00　10　30　55</td>
+              <td>17</td>
+              <td>10　45</td>
+            </tr>
+            <tr>
+              <td>11</td>
+              <td>00　25　50</td>
+              <td>18</td>
+              <td>05　35</td>
+            </tr>
+            <tr>
+              <td>12</td>
+              <td>25</td>
+              <td>19</td>
+              <td>35</td>
+            </tr>
+            <tr>
+              <td>13</td>
+              <td>35</td>
+              <td>20</td>
+              <td>25</td>
+            </tr>
+            <tr>
+              <td>14</td>
+              <td>05　35</td>
+              <td>21</td>
+              <td>05</td>
+            </tr>
+          </tbody>
         </table>
 
         <table data-active={mode === 'C'}>
-          <tr>
-            <th>時</th>
-            <th>分</th>
-            <th>時</th>
-            <th>分</th>
-          </tr>
-          <tr>
-            <td width="10%">8</td>
-            <td width="40%">10　35</td>
-            <td width="10%">15</td>
-            <td width="40%">05　35</td>
-          </tr>
-          <tr>
-            <td>9</td>
-            <td>00　25　50</td>
-            <td>16</td>
-            <td>05　45</td>
-          </tr>
-          <tr>
-            <td>10</td>
-            <td>10　55</td>
-            <td>17</td>
-            <td>10　45</td>
-          </tr>
-          <tr>
-            <td>11</td>
-            <td>25　50</td>
-            <td>18</td>
-            <td>05　35</td>
-          </tr>
-          <tr>
-            <td>12</td>
-            <td>25</td>
-            <td>19</td>
-            <td>35</td>
-          </tr>
-          <tr>
-            <td>13</td>
-            <td>35</td>
-            <td>20</td>
-            <td>25</td>
-          </tr>
-          <tr>
-            <td>14</td>
-            <td>05　35</td>
-            <td>21</td>
-            <td>05</td>
-          </tr>
+          <thead>
+            <tr>
+              <th>時</th>
+              <th>分</th>
+              <th>時</th>
+              <th>分</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <td width="10%">8</td>
+              <td width="40%">10　35</td>
+              <td width="10%">15</td>
+              <td width="40%">05　35</td>
+            </tr>
+            <tr>
+              <td>9</td>
+              <td>00　25　50</td>
+              <td>16</td>
+              <td>05　45</td>
+            </tr>
+            <tr>
+              <td>10</td>
+              <td>10　55</td>
+              <td>17</td>
+              <td>10　45</td>
+            </tr>
+            <tr>
+              <td>11</td>
+              <td>25　50</td>
+              <td>18</td>
+              <td>05　35</td>
+            </tr>
+            <tr>
+              <td>12</td>
+              <td>25</td>
+              <td>19</td>
+              <td>35</td>
+            </tr>
+            <tr>
+              <td>13</td>
+              <td>35</td>
+              <td>20</td>
+              <td>25</td>
+            </tr>
+            <tr>
+              <td>14</td>
+              <td>05　35</td>
+              <td>21</td>
+              <td>05</td>
+            </tr>
+          </tbody>
         </table>
       </section>
 
@@ -186,156 +201,168 @@ export default function TimeTable() {
         <h3>時刻表</h3>
 
         <table data-active={mode === 'A'}>
-          <tr>
-            <th>時</th>
-            <th>分</th>
-            <th>時</th>
-            <th>分</th>
-          </tr>
-          <tr>
-            <td width="10%">8</td>
-            <td width="40%">20　50</td>
-            <td width="10%">15</td>
-            <td width="40%">00　10　20　30　40　50</td>
-          </tr>
-          <tr>
-            <td>9</td>
-            <td>20　50</td>
-            <td>16</td>
-            <td>00　05　10　15　25　30　35　40　45　50　55</td>
-          </tr>
-          <tr>
-            <td>10</td>
-            <td>20 50</td>
-            <td>17</td>
-            <td>00　10　15　20　25　30　35　40　45　55</td>
-          </tr>
-          <tr>
-            <td>11</td>
-            <td>00　10　20　30　40　50</td>
-            <td>18</td>
-            <td>00　10　20　30　40　50</td>
-          </tr>
-          <tr>
-            <td>12</td>
-            <td>00　10　20　30　40　50</td>
-            <td>19</td>
-            <td>00　15　30　45</td>
-          </tr>
-          <tr>
-            <td>13</td>
-            <td>00　15　30　45</td>
-            <td>20</td>
-            <td>00　15　30　45</td>
-          </tr>
-          <tr>
-            <td>14</td>
-            <td>00　10　20　30　40　45　50　55</td>
-            <td>21</td>
-            <td>00　15　30　45</td>
-          </tr>
+          <thead>
+            <tr>
+              <th>時</th>
+              <th>分</th>
+              <th>時</th>
+              <th>分</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <td width="10%">8</td>
+              <td width="40%">20　50</td>
+              <td width="10%">15</td>
+              <td width="40%">00　10　20　30　40　50</td>
+            </tr>
+            <tr>
+              <td>9</td>
+              <td>20　50</td>
+              <td>16</td>
+              <td>00　05　10　15　25　30　35　40　45　50　55</td>
+            </tr>
+            <tr>
+              <td>10</td>
+              <td>20　50</td>
+              <td>17</td>
+              <td>00　10　15　20　25　30　35　40　45　55</td>
+            </tr>
+            <tr>
+              <td>11</td>
+              <td>00　10　20　30　40　50</td>
+              <td>18</td>
+              <td>00　10　20　30　40　50</td>
+            </tr>
+            <tr>
+              <td>12</td>
+              <td>00　10　20　30　40　50</td>
+              <td>19</td>
+              <td>00　15　30　45</td>
+            </tr>
+            <tr>
+              <td>13</td>
+              <td>00　15　30　45</td>
+              <td>20</td>
+              <td>00　15　30　45</td>
+            </tr>
+            <tr>
+              <td>14</td>
+              <td>00　10　20　30　40　45　50　55</td>
+              <td>21</td>
+              <td>00　15　30　45</td>
+            </tr>
+          </tbody>
         </table>
 
         <table data-active={mode === 'B'}>
-          <tr>
-            <th>時</th>
-            <th>分</th>
-            <th>時</th>
-            <th>分</th>
-          </tr>
-          <tr>
-            <td width="10%">8</td>
-            <td width="40%">50</td>
-            <td width="10%">15</td>
-            <td width="40%">20　50</td>
-          </tr>
-          <tr>
-            <td>9</td>
-            <td>40</td>
-            <td>16</td>
-            <td>20</td>
-          </tr>
-          <tr>
-            <td>10</td>
-            <td>05　50</td>
-            <td>17</td>
-            <td>00　30　55</td>
-          </tr>
-          <tr>
-            <td>11</td>
-            <td>15　40</td>
-            <td>18</td>
-            <td>20　50</td>
-          </tr>
-          <tr>
-            <td>12</td>
-            <td>10</td>
-            <td>19</td>
-            <td>20　50</td>
-          </tr>
-          <tr>
-            <td>13</td>
-            <td>20　50</td>
-            <td>20</td>
-            <td>40</td>
-          </tr>
-          <tr>
-            <td>14</td>
-            <td>20　50</td>
-            <td>21</td>
-            <td>30</td>
-          </tr>
+          <thead>
+            <tr>
+              <th>時</th>
+              <th>分</th>
+              <th>時</th>
+              <th>分</th>
+            </tr>
+            <tr>
+              <td width="10%">8</td>
+              <td width="40%">50</td>
+              <td width="10%">15</td>
+              <td width="40%">20　50</td>
+            </tr>
+            <tr>
+              <td>9</td>
+              <td>40</td>
+              <td>16</td>
+              <td>20</td>
+            </tr>
+            <tr>
+              <td>10</td>
+              <td>05　50</td>
+              <td>17</td>
+              <td>00　30　55</td>
+            </tr>
+            <tr>
+              <td>11</td>
+              <td>15　40</td>
+              <td>18</td>
+              <td>20　50</td>
+            </tr>
+            <tr>
+              <td>12</td>
+              <td>10</td>
+              <td>19</td>
+              <td>20　50</td>
+            </tr>
+            <tr>
+              <td>13</td>
+              <td>20　50</td>
+              <td>20</td>
+              <td>40</td>
+            </tr>
+            <tr>
+              <td>14</td>
+              <td>20　50</td>
+              <td>21</td>
+              <td>30</td>
+            </tr>
+          </thead>
         </table>
 
         <table data-active={mode === 'C'}>
-          <tr>
-            <th>時</th>
-            <th>分</th>
-            <th>時</th>
-            <th>分</th>
-          </tr>
-          <tr>
-            <td width="10%">8</td>
-            <td width="40%">50</td>
-            <td width="10%">15</td>
-            <td width="40%">20　50</td>
-          </tr>
-          <tr>
-            <td>9</td>
-            <td>40</td>
-            <td>16</td>
-            <td>20</td>
-          </tr>
-          <tr>
-            <td>10</td>
-            <td>05　50</td>
-            <td>17</td>
-            <td>00　30　55</td>
-          </tr>
-          <tr>
-            <td>11</td>
-            <td>15　40</td>
-            <td>18</td>
-            <td>20　50</td>
-          </tr>
-          <tr>
-            <td>12</td>
-            <td>10</td>
-            <td>19</td>
-            <td>20　50</td>
-          </tr>
-          <tr>
-            <td>13</td>
-            <td>20　50</td>
-            <td>20</td>
-            <td>40</td>
-          </tr>
-          <tr>
-            <td>14</td>
-            <td>20　50</td>
-            <td>21</td>
-            <td>30</td>
-          </tr>
+          <thead>
+            <tr>
+              <th>時</th>
+              <th>分</th>
+              <th>時</th>
+              <th>分</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <td width="10%">8</td>
+              <td width="40%">50</td>
+              <td width="10%">15</td>
+              <td width="40%">20　50</td>
+            </tr>
+            <tr>
+              <td>9</td>
+              <td>40</td>
+              <td>16</td>
+              <td>20</td>
+            </tr>
+            <tr>
+              <td>10</td>
+              <td>05　50</td>
+              <td>17</td>
+              <td>00　30　55</td>
+            </tr>
+            <tr>
+              <td>11</td>
+              <td>15　40</td>
+              <td>18</td>
+              <td>20　50</td>
+            </tr>
+            <tr>
+              <td>12</td>
+              <td>10</td>
+              <td>19</td>
+              <td>20　50</td>
+            </tr>
+            <tr>
+              <td>13</td>
+              <td>20　50</td>
+              <td>20</td>
+              <td>40</td>
+            </tr>
+            <tr>
+              <td>14</td>
+              <td>20　50</td>
+              <td>21</td>
+              <td>30</td>
+            </tr>
+          </tbody>
         </table>
       </section>
     </>
